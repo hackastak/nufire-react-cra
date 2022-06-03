1e551f7e741073275913d14b41aa3af9d0d8a9f5
Austin Wiginton <ahwig93@gmail.com>
2022-06-02 21:56:35 -0500

    Create basic form of the site including the creation of the base Navbar component and add in the logo and favicon

---
+++ src/components/layouts/NuFireLogo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logoImg from './assets/NuFire_logo.png';
+
+function NuFireLogo() {
+  return (
+    <div className="w-60 mx-5 my-5">
+      <img width={50} src={logoImg} alt="NuFire Logo" />
+    </div>
+  )
+}
+
+export default NuFireLogo;
